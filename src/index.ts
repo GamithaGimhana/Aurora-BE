47040f701e909a8a10ce3ed7c8d178197c0d1a82
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-11-26 12:15:08 +0530

    project initialization

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db";
+
+dotenv.config();
+
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+
+const app = express();
+
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "Backend running..." });
+});
+
+// Start server
+const startServer = async () => {
+  await connectDB();
+  app.listen(SERVER_PORT, () =>
+    console.log(`Server running on port ${SERVER_PORT}`)
+  );
+};
+
+startServer();
