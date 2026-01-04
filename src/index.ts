67ba5945da1d1f92dbc223e4cfeac53474d56cd5
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-04 20:29:38 +0530

    Refactor CORS configuration to use a static origin array and simplify origin handling

--- src/index.ts
+++ src/index.ts
@@ -22,56 +22,41 @@ const SERVER_PORT = process.env.SERVER_PORT || 5000;
 const app = express();
 
 // CORS
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://aurora-fe-eight.vercel.app",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // Allow tools like Postman
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error("Not allowed by CORS"));
-      }
-      return callback(null, true);
-    },
+    // origin: ["http://localhost:5173"],
+    origin: ['http://localhost:5173', 'https://aurora-fe-eight.vercel.app'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// Handle OPTIONS preflight requests
-app.options("*", cors());
-
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-// Public routes
-app.use("/api/v1/auth", authRoutes);
+// Routes
+app.use('/api/v1/auth', authRoutes)
 
-// Protected routes
 app.use(authenticate);
 
-app.use("/api/v1/admin", adminRoutes);
-app.use("/api/v1/notes", noteRoutes);
-app.use("/api/v1/flashcards", flashcardRoutes);
-app.use("/api/v1/questions", questionRoutes);
-app.use("/api/v1/quizzes", quizRoutes);
-app.use("/api/v1/rooms", quizRoomRoutes);
-app.use("/api/v1/attempts", attemptRoutes);
+app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1/notes', noteRoutes);
+app.use('/api/v1/flashcards', flashcardRoutes);
+app.use('/api/v1/questions', questionRoutes);
+app.use('/api/v1/quizzes', quizRoutes);
+app.use('/api/v1/rooms', quizRoomRoutes);
+app.use('/api/v1/attempts', attemptRoutes);
 
-// Error handler
 app.use(errorHandler);
 
-// -Test route
+// Test route
 app.get("/", (req, res) => {
   res.json({ message: "Backend running..." });
 });
 
-// Start Server
+// Start server
 const startServer = async () => {
   await connectDB();
   await createDefaultAdmin();
